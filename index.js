a344aac0410722a1ab9a69ff1f6a4dfd5f87267d
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-11-29 01:46:42 +0500

    Appropriate return message for number type inputs

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ module.exports = function(inputString) {
                 return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             }
         );
-    } else {
+    } else if ( typeof inputString === 'number') {
+        return 'Incompatible input type';
+    }else {
         return null;
     }
 };
